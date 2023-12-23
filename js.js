f1d95e08e03b4128558aa412ff560b58e07b38fb
rohitGupta0409 <136116960+rohitGupta0409@users.noreply.github.com>
2023-12-23 23:09:23 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,27 +1,16 @@
 // js.js
 
-// Function to extract Lua code snippet from HTML content
-function extractLuaCode(htmlContent) {
-  var beginComment = "<!-- BEGIN LUA CODE -->";
-  var endComment = "<!-- END LUA CODE -->";
-
-  var startIndex = htmlContent.indexOf(beginComment) + beginComment.length;
-  var endIndex = htmlContent.indexOf(endComment);
-
-  if (startIndex !== -1 && endIndex !== -1) {
-    return htmlContent.substring(startIndex, endIndex);
-  } else {
-    return "Code snippet not found.";
-  }
+// Function to return Lua code
+function getLuaCode() {
+  return `
+    -- Your Lua code for Roblox
+    local part = Instance.new("Part")
+    part.Size = Vector3.new(10, 5, 3)
+    part.Anchored = true
+    part.Position = Vector3.new(0, 10, 0)
+    part.Parent = workspace
+  `;
 }
 
-// Fetch the HTML content and display Lua code
-fetch("index.html")
-  .then(response => response.text())
-  .then(htmlContent => {
-    var luaCode = extractLuaCode(htmlContent);
-    document.getElementById("luaCodePlaceholder").textContent = luaCode;
-
-    // You can perform further actions with the Lua code here
-  })
-  .catch(error => console.error("Error fetching HTML:", error));
+// Return the Lua code
+getLuaCode();
