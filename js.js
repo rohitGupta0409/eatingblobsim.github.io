c83e20497028bd8e7b838295a03c37cadeea081a
rohitGupta0409 <136116960+rohitGupta0409@users.noreply.github.com>
2023-12-23 23:12:42 +0530

    Update js.js

--- js.js
+++ js.js
@@ -3,12 +3,13 @@
 // Function to return Lua code
 function getLuaCode() {
   return `
-    -- Your Lua code for Roblox
-    local part = Instance.new("Part")
-    part.Size = Vector3.new(10, 5, 3)
-    part.Anchored = true
-    part.Position = Vector3.new(0, 10, 0)
-    part.Parent = workspace
+print("working")
+
+game.Players.PlayerAdded:Connect(function(player)
+	player.CharacterAdded:Connect(function(char)
+		print(char.Name)
+	end)
+end)
   `;
 }
 
