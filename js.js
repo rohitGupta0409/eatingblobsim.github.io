b28655899184fb9f580bce0ab41c6d8c17cf5720
rohitGupta0409 <136116960+rohitGupta0409@users.noreply.github.com>
2023-12-23 23:00:29 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,20 +1,27 @@
-local HttpService = game:GetService("HttpService")
+// js.js
 
--- Replace 'your-website-url' with the actual URL of your GitHub Pages repository
-local apiUrl = "https://rohitgupta0409.github.io/eatingblobsim.github.io/index.html"
+// Function to extract Lua code snippet from HTML content
+function extractLuaCode(htmlContent) {
+  var beginComment = "<!-- BEGIN LUA CODE -->";
+  var endComment = "<!-- END LUA CODE -->";
 
-local success, response = pcall(function()
-    return HttpService:GetAsync(apiUrl)
-end)
+  var startIndex = htmlContent.indexOf(beginComment) + beginComment.length;
+  var endIndex = htmlContent.indexOf(endComment);
 
-if success then
-    print("Request successful")
+  if (startIndex !== -1 && endIndex !== -1) {
+    return htmlContent.substring(startIndex, endIndex);
+  } else {
+    return "Code snippet not found.";
+  }
+}
 
-    -- Extract the code from the response
-    local codeSnippet = response:match('<code>(.-)</code>')
+// Fetch the HTML content and extract Lua code
+fetch("index.html")
+  .then(response => response.text())
+  .then(htmlContent => {
+    var luaCode = extractLuaCode(htmlContent);
+    console.log("Lua Code:", luaCode);
 
-    print("Code snippet:")
-    print(codeSnippet)
-else
-    print("Request failed:", response)
-end
+    // You can perform further actions with the Lua code here
+  })
+  .catch(error => console.error("Error fetching HTML:", error));
